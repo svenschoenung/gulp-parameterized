6ed035ae50fe65455e590490c89973ab9867794e
Sven Schoenung <sven@taz.local>
2016-11-12 21:14:21 +0100

    Initial implementation

---
+++ index.js
@@ -0,0 +1,33 @@
+var _ = require('lodash');
+var yargs = require('yargs');
+
+var requireGulp = require('./lib/util/requireGulp.js');
+
+var createTask = require('./lib/task.js');
+var createComposition = require('./lib/composition.js');
+
+function createInstance(config) {
+
+  var gulp = config.gulp;
+  config.gulp = _.isFunction(gulp) ? gulp : function() { return gulp; };
+
+  var instance = createTask(config);
+
+  instance.task = instance;
+  instance.series = createComposition(config, instance, 'series');
+  instance.parallel = createComposition(config, instance, 'parallel');
+
+  instance.withGulp = function(gulp) {
+    return createInstance(_.assign({}, config, {gulp: gulp}));
+  };
+  instance.withOptions = function(options) {
+    return createInstance(_.assign({}, config, options));
+  };
+
+  return instance;
+}
+
+module.exports = createInstance({
+  gulp: requireGulp,
+  argv: yargs.argv
+});
