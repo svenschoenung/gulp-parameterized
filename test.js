27f28f393e23ea5313e559e465de260507964e0c
Sven Schoenung <sven.schoenung@gmail.com>
2016-11-24 21:40:46 +0100

    Refactor parameterized.task() tests

--- test.js
+++ test.js
@@ -10,228 +10,226 @@ function requireUncached(mod) {
   return require(mod);
 }
 
+function init(opts) {
+  opts = opts || { argv: {} };
+  return requireUncached('./index.js').withOptions(opts);
+}
+
+function testCallingTask(config) {
+  var parameterized = init({ argv: config.argv || {} });
+  var gulp = requireUncached('gulp');
+
+  function resultCallback(r) { config.actual = r; }
+
+  config.setup(gulp, parameterized, resultCallback);
+
+  gulp.series('task')(function() {
+    delete config.actual['_'];
+    delete config.actual['$0'];
+    expect(config.actual).to.eql(config.expected);
+    config.callback();
+  });
+}
+
+function testCallingOtherTask(config) {
+  var gulp = requireUncached('gulp');
+  var parameterized = init();
+  
+  gulp.task(parameterized('otherTask', function(done, params) {
+    config.actual = params;
+    done();
+  }));
+
+  gulp.task('task', parameterized.apply(null, config.call));
+
+  gulp.series('task')(function() {
+    delete config.actual['_'];
+    delete config.actual['$0'];
+    expect(config.actual).to.eql(config.expected);
+    config.callback();
+  });
+}
+
 describe('parameterized.task()', function() {
 
-  it('provides regular callback', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js');
-    var taskExecuted = false;
-    gulp.task('task1', parameterized(function(done) {
-      taskExecuted = true;
-      done();
-    }));
-    gulp.series('task1')(function() {
-      expect(taskExecuted).to.equal(true);
-      testDone();
+  it('provides a regular callback', function(cb) {
+    testCallingTask({
+      setup: function(gulp, parameterized, result) {
+        gulp.task('task', parameterized(function(done) {
+          result('taskCalled');
+          done();
+        }));
+      },
+      expected: 'taskCalled',  
+      callback: cb
     });
   });
 
-  it('provides _.done() callback', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js');
-    var taskExecuted = false;
-    gulp.task('task2', parameterized(function(_) {
-      taskExecuted = true;
-      _.done();
-    }));
-    gulp.series('task2')(function() {
-      expect(taskExecuted).to.equal(true);
-      testDone();
+  it('provides a _.done() callback', function(cb) {
+    testCallingTask({
+      setup: function(gulp, parameterized, result) {
+        gulp.task('task', parameterized(function(_) {
+          result('taskCalled');
+          _.done();
+        }));
+      },
+      expected: 'taskCalled',  
+      callback: cb
     });
   });
 
-  it('provides parsed command line params in _.params', function(done) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js').withOptions({
-      argv: { 'param': 'foo' }
-    });
-    var providedParams;
-    gulp.task('task3', parameterized(function(_) {
-      providedParams = _.params;
-      _.done();
-    }));
-    gulp.series('task3')(function() {
-      expect(providedParams).to.eql({ param: 'foo'});
-      done();
+  it('provides params in _.params', function(cb) {
+    testCallingTask({
+      argv: { param: 'foo' },
+      setup: function(gulp, parameterized, result) {
+        gulp.task('task', parameterized(function(_) {
+          result(_.params);
+          _.done();
+        }));
+      },
+      expected: { param: 'foo' },  
+      callback: cb
     });
   });
 
-  it('provides parsed command line params in second argument', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js').withOptions({
-      argv: { 'param': 'foo' }
-    });
-    var providedParams;
-    gulp.task('task4', parameterized(function(done, params) {
-      providedParams = params;
-      done();
-    }));
-    gulp.series('task4')(function() {
-      expect(providedParams).to.eql({ param: 'foo'});
-      testDone();
+  it('provides params in second argument', function(cb) {
+    testCallingTask({
+      argv: { param: 'foo' },
+      setup: function(gulp, parameterized, result) {
+        gulp.task('task', parameterized(function(done, params) {
+          result(params);
+          done();
+        }));
+      },
+      expected: { param: 'foo' },  
+      callback: cb
     });
   });
 
-  it('accepts default params that can be overwritten by command line params', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js').withOptions({
-      argv: { 'param2': 'baz', 'param3': 'quux' }
-    });
-    var providedParams;
-    gulp.task('task5', parameterized(function(done, params) {
-      providedParams = params;
-      done();
-    }, { param1: 'foo', param2: 'bar' }));
-    gulp.series('task5')(function() {
-      expect(providedParams).to.eql({
-        param1: 'foo',
-        param2: 'baz',
-        param3: 'quux'
-      });
-      testDone();
+  it('accepts default params as object', function(cb) {
+    testCallingTask({
+      argv: { 'p2': 'v2', 'p3': 'v3' },
+      setup: function(gulp, parameterized, result) {
+        gulp.task('task', parameterized(function(done, params) {
+          result(params);
+          done();
+        }, { p1: 'v1', p2: 'XXX' }));
+      },
+      expected: { p1: 'v1', p2: 'v2', p3: 'v3' },  
+      callback: cb
     });
   });
 
-  it('accepts default params that can be overwritten by command line params', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js').withOptions({
-      argv: { 'param2': 'baz', 'param3': 'quux' }
-    });
-    var providedParams;
-    gulp.task('task5', parameterized(function(done, params) {
-      providedParams = params;
-      done();
-    }, { param1: 'foo', param2: 'bar' }));
-    gulp.series('task5')(function() {
-      expect(providedParams).to.eql({
-        param1: 'foo',
-        param2: 'baz',
-        param3: 'quux'
-      });
-      testDone();
+  it('accepts default params as array', function(cb) {
+    testCallingTask({
+      argv: { 'p2': 'v2', 'p3': 'v3' },
+      setup: function(gulp, parameterized, result) {
+        gulp.task('task', parameterized(function(done, params) {
+          result(params);
+          done();
+        }, ['--p1', 'v1', '--p2', 'XXX']));
+      },
+      expected: { p1: 'v1', p2: 'v2', p3: 'v3' },  
+      callback: cb
     });
   });
 
-  it('accepts a name and default params', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js').withOptions({
-      argv: { 'param2': 'baz', 'param3': 'quux' }
-    });
-    var providedParams;
-    gulp.task(parameterized('task6', function(done, params) {
-      providedParams = params;
-      done();
-    }, { param1: 'foo', param2: 'bar' }));
-    gulp.series('task6')(function() {
-      expect(providedParams).to.eql({
-        param1: 'foo',
-        param2: 'baz',
-        param3: 'quux'
-      });
-      testDone();
+  it('accepts default params as string', function(cb) {
+    testCallingTask({
+      argv: { 'p2': 'v2', 'p3': 'v3' },
+      setup: function(gulp, parameterized, result) {
+        gulp.task('task', parameterized(function(done, params) {
+          result(params);
+          done();
+        }, '--p1 v1 --p2 XXX'));
+      },
+      expected: { p1: 'v1', p2: 'v2', p3: 'v3' },  
+      callback: cb
     });
   });
 
-  it('accepts a name and default params', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js');
-    var taskExecuted = false;
-    gulp.task(parameterized('task7', function(done, params) {
-      taskExecuted = true;
-      done();
-    }));
-    gulp.series('task7')(function() {
-      expect(taskExecuted).to.equal(true);
-      testDone();
+  it('accepts name and default params as object', function(cb) {
+    testCallingTask({
+      argv: { 'p2': 'v2', 'p3': 'v3' },
+      setup: function(gulp, parameterized, result) {
+        gulp.task(parameterized('task', function(done, params) {
+          result(params);
+          done();
+        }, { p1: 'v1', p2: 'XXX' }));
+      },
+      expected: { p1: 'v1', p2: 'v2', p3: 'v3' },  
+      callback: cb
     });
   });
 
-  it('can call another task with a name and params object', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js').withOptions({
-      argv: {}
-    });
-    var providedParams; 
-    gulp.task(parameterized('task8', function(done, params) {
-      providedParams = params;
-      done();
-    }));
-    gulp.task('task9', parameterized('task8', {
-      param1: 'foo', param2: 'bar'
-    }));
-    gulp.series('task9')(function() {
-      expect(providedParams).to.eql({ param1: 'foo', param2: 'bar' });
-      testDone();
+  it('accepts name and default params as array', function(cb) {
+    testCallingTask({
+      argv: { 'p2': 'v2', 'p3': 'v3' },
+      setup: function(gulp, parameterized, result) {
+        gulp.task(parameterized('task', function(done, params) {
+          result(params);
+          done();
+        }, ['--p1', 'v1', '--p2', 'XXX']));
+      },
+      expected: { p1: 'v1', p2: 'v2', p3: 'v3' },  
+      callback: cb
     });
   });
 
-  it('can call another task with a name and params array', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js').withOptions({
-      argv: {}
-    });
-    var providedParams; 
-    gulp.task(parameterized('task10', function(done, params) {
-      providedParams = params;
-      done();
-    }));
-    gulp.task('task11', parameterized('task10', [
-      '--param1', 'foo', '--param2', 'bar'
-    ]));
-    gulp.series('task11')(function() {
-      delete providedParams['_'];
-      delete providedParams['$0'];
-      expect(providedParams).to.eql({ param1: 'foo', param2: 'bar' });
-      testDone();
+  it('accepts name and default params as string', function(cb) {
+    testCallingTask({
+      argv: { 'p2': 'v2', 'p3': 'v3' },
+      setup: function(gulp, parameterized, result) {
+        gulp.task(parameterized('task', function(done, params) {
+          result(params);
+          done();
+        }, '--p1 v1 --p2 XXX'));
+      },
+      expected: { p1: 'v1', p2: 'v2', p3: 'v3' },  
+      callback: cb
     });
   });
 
-  it('can call another task with a name and and params string', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js').withOptions({
-      argv: {}
-    });
-    var providedParams; 
-    gulp.task(parameterized('task12', function(done, params) {
-      providedParams = params;
-      done();
-    }));
-    gulp.task('task13', parameterized('task12', '--param1 foo --param2 bar'));
-    gulp.series('task13')(function() {
-      delete providedParams['_'];
-      delete providedParams['$0'];
-      expect(providedParams).to.eql({ param1: 'foo', param2: 'bar' });
-      testDone();
+  it('can call another task with a name and params object', function(cb) {
+    testCallingOtherTask({
+      call: ['otherTask', { param1: 'foo', param2: 'bar' }],
+      expected: { param1: 'foo', param2: 'bar' },
+      callback: cb
     });
   });
 
-  it('can call another task with a single params string', function(testDone) {
-    var gulp = requireUncached('gulp');
-    var parameterized = requireUncached('./index.js').withOptions({
-      argv: {}
-    });
-    var providedParams; 
-    gulp.task(parameterized('task10', function(done, params) {
-      providedParams = params;
-      done();
-    }));
-    gulp.task('task11', parameterized('task10 --param1 foo --param2 bar'));
-    gulp.series('task11')(function() {
-      delete providedParams['_'];
-      delete providedParams['$0'];
-      expect(providedParams).to.eql({ param1: 'foo', param2: 'bar' });
-      testDone();
+  it('can call another task with a name and params array', function(cb) {
+    testCallingOtherTask({
+      call: ['otherTask', ['--param1', 'foo', '--param2', 'bar']],
+      expected: { param1: 'foo', param2: 'bar' },
+      callback: cb
     });
   });
 
-  it('fails for invalid arguments', function(testDone) {
+  it('can call another task with a name and params string', function(cb) {
+    testCallingOtherTask({
+      call: ['otherTask', '--param1 foo --param2 bar'],
+      expected: { param1: 'foo', param2: 'bar' },
+      callback: cb
+    });
+  });
+
+  it('can call another task with a params string', function(cb) {
+    testCallingOtherTask({
+      call: ['otherTask --param1 foo --param2 bar'],
+      expected: { param1: 'foo', param2: 'bar' },
+      callback: cb
+    });
+  });
+
+  it('fails for invalid arguments', function(cb) {
     var gulp = requireUncached('gulp');
     var parameterized = requireUncached('./index.js');
     expect(parameterized.bind(null, 1, 1, 1)).to.throw(Error);
     expect(parameterized.bind(null, 1, 1)).to.throw(Error);
     expect(parameterized.bind(null, 1)).to.throw(Error);
     expect(parameterized.bind(null)).to.throw(Error);
-    testDone();
+    cb();
   });
 
 
